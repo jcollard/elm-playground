ce02e8306593a17e4f5bed5c0d6db393c29bf5b5
jcollard <josephmcollard@gmail.com>
2014-12-03 11:20:52 -0500

    Removed @docs on non-existent elements.

--- src/Playground/Input.elm
+++ src/Playground/Input.elm
@@ -1,10 +1,11 @@
-module Playground.Input (..) where
+module Playground.Input where
 
 {-|
+
   This module explains each type of Input that can be used in a Playground's
   update function.
 
-  @docs RealWorld, Input, Key, Direction
+  @docs RealWorld, Input
 
  -}
 
@@ -26,22 +27,27 @@ type alias RealWorld = { top : Float
                        , right : Float
                        , bottom : Float
                        , left : Float
-                       , mouse : { x : Float, y : Float } }
+                       , mouse : { x : Float, y : Float } 
+                       }
 
 
 {-|
+
   Inputs are passed to a Playground's update function. If multiple inputs are
   generated, the update function is called multiple times accumulating all of
   the changes. Your update function will always be passed a Passive input and
   it is guaranteed that the Passive input will occur before all other inputs.
 
   * Tap k - Fires once when `k` is pressed
+
   * Key k - Fires every time the update function is called while `k` is pressed
+
   * MouseUp - Fires once when the left mouse button is released
+
   * MouseDown - Fires every time the update function is called while the mouse is down
+
   * Passive t - This input is always passed to the update function and expresses
     how many milliseconds have passed since the last update was accumulated.
-
 
  -}
 type Input = Tap Keys.Key
